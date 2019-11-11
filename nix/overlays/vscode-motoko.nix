aa8f40ecef3cb5e2a41cfd023b8e192fd2f89f52
David Millar-Durrant <dmillardurrant@gmail.com>
2019-11-11 16:38:45 +0100

    Update to vscode-motoko master

--- nix/overlays/vscode-motoko.nix
+++ nix/overlays/vscode-motoko.nix
@@ -2,8 +2,8 @@ self: super:
 
 let src = builtins.fetchGit {
   url = "ssh://git@github.com/dfinity-lab/vscode-motoko";
-  ref = "nix-from-scratch";
-  rev = "53a65af37a0b369f324f71eb96914a597b04c4c3";
+  ref = "master";
+  rev = "dec6f40bab5b0321be1b58c0c0d1808b91f1c835";
 }; in
 {
   vscode-motoko = import src { pkgs = self.pkgs; };
